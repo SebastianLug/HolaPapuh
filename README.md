92a84a6e1001f942ba26cf0b655dd686f603b4b1
SebastianLug <aslugmana@espe.edu.ec>
2024-11-26 18:48:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HolaPapuh
+ HolaMundo
---
+++ src/holapapuh/HolaPapuh.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holapapuh;
+
+/**
+ *
+ * @author sbeas
+ */
+public class HolaPapuh {
+
+    public static void main(String[] args) {
+        // Imprime "Hola Mundo" en la consola
+        System.out.println("Hola Mundo");
+    }
+}
