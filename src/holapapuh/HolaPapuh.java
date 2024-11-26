fd825b1366e8367b585bee10b44840024975267e
SebastianLug <aslugmana@espe.edu.ec>
2024-11-26 18:58:53 -0500

    Se agrego un mensaje
    solo un mensaje

--- src/holapapuh/HolaPapuh.java
+++ src/holapapuh/HolaPapuh.java
@@ -13,5 +13,6 @@ public class HolaPapuh {
     public static void main(String[] args) {
         // Imprime "Hola Mundo" en la consola
         System.out.println("Hola Mundo");
+        System.out.println("Como estas Papi?");
     }
 }
